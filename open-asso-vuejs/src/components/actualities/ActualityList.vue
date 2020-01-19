bbab570b04ac4875f01bd62f4b92288650d5c866
Eliott Legrand <e-legrand59@hotmail.fr>
2020-01-19 22:39:12 +0100

    update actualities

--- open-asso-vuejs/src/components/actualities/ActualityList.vue
+++ open-asso-vuejs/src/components/actualities/ActualityList.vue
@@ -1,35 +1,54 @@
 <template>
   <div>
-    <TopBar title="Matchs"></TopBar>
+    <TopBar title="Actualités"></TopBar>
 
     <v-content>
         <v-container fluid>
-            <v-row align="center" justify="center">
-            <v-col>
-                <v-list three-line>
-                    <v-list-item>
-                        <v-subheader>Janvier 2020</v-subheader>
-                    </v-list-item>
-                    <v-list-item>
-                        <v-list-item-content>
-                            <v-list-item-title>Diapos de la soirée de Noël</v-list-item-title>
-                            <v-list-item-subtitle>Écrit le 10/01/20, par Allan</v-list-item-subtitle>
-                            <v-list-item-subtitle>Merci encore à toutes et à tous d'être venu participer à la soirée au ...</v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-list-item>
-                    <v-list-item>
-                        <v-subheader>Novembre 2019</v-subheader>
-                    </v-list-item>
-                    <v-list-item>
-                        <v-list-item-content>
-                            <v-list-item-title>Félicitations à l'équipe des Juniors pour leur arrivée en Finale !</v-list-item-title>
-                            <v-list-item-subtitle>Écrit le 20/11/19, par Allan</v-list-item-subtitle>
-                            <v-list-item-subtitle>Encore bravo à l'équipe des Juniors et leur entraineur, ...</v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-list-item>
-                </v-list>
-            </v-col>
+            <v-row key="0">
+                <v-col md="4" cols="6" class="pa-3 d-flex flex-column">
+                <v-card class="elevation-5 flex d-flex flex-column">
+                    <v-img
+                        src="@/assets/LogoApp.png"
+                        height="125"
+                        class="grey darken-4"
+                    ></v-img>
+                    <v-card-title style="word-break:normal" class="title">Diapos de la soirée de Noël</v-card-title>
+                    <v-card-text style="word-break:normal" class="text--primary flex">
+                        <div>Écrit le 10/01/20, par Allan</div>
+                        <div>Encore merci à tous !</div>
+                    </v-card-text>
+                </v-card>
+                </v-col>
+
+                <v-col md="4" cols="6" class="pa-3 d-flex flex-column">
+                <v-card class="elevation-5 flex d-flex flex-column">
+                    <v-img
+                    src="@/assets/LogoApp.png"
+                    height="125"
+                    class="grey darken-4"
+                    ></v-img>
+                    <v-card-title class="title">Bonne année !</v-card-title>
+                    <v-card-text style="word-break:normal" class="text--primary flex">
+                        <div>Écrit le 01/01/20, par Allan</div>
+                    </v-card-text>
+                </v-card>
+                </v-col>
+
+                <v-col md="4" cols="6" class="pa-3 d-flex flex-column">
+                <v-card class="elevation-5 flex d-flex flex-column">
+                    <v-img
+                    src="@/assets/LogoApp.png"
+                    height="125"
+                    class="grey darken-4"
+                    ></v-img>
+                    <v-card-title class="title">Nouvelle rentrée</v-card-title>
+                    <v-card-text style="word-break:normal" class="text--primary flex">
+                            <div>Écrit le 07/09/19, par Allan</div>
+                    </v-card-text>
+                </v-card>
+                </v-col>
             </v-row>
+
         </v-container>
     </v-content>
   </div>
