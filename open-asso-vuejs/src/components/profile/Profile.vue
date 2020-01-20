fd8ed7dcbe67c3d9617ac55105c17d1a83c7ca52
Eliott Legrand <e-legrand59@hotmail.fr>
2020-01-20 18:14:21 +0100

    fix profile width

--- open-asso-vuejs/src/components/profile/Profile.vue
+++ open-asso-vuejs/src/components/profile/Profile.vue
@@ -5,33 +5,24 @@
   <div>
     <TopBar title="Mon Profil" back></TopBar>
     <v-content>
-      <v-container fill-height fluid>
+      <v-container fluid>
+        
         <v-row align="center" justify="center">
-          <v-col :cols="2"></v-col>
-          <v-col class="text-center" :cols="8">
+          <v-col class="text-center" style="width: 100%" cols="12" sm="4">
             <v-avatar>
               <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
             </v-avatar>
 
-            <div>Jonathan Lee</div>
+            <div class="my-2">Jonathan Lee</div>
 
-            <div>membre depuis le 29/08/2018</div>
-            <v-list>
-              <v-list-item>
-                <v-btn color="#1e35b4" min-width="200"><span class="white--text">Modifier la photo</span></v-btn>
-              </v-list-item>
-              <v-list-item>
-                <v-btn color="#1e35b4" min-width="200" link :to="docPath"><span class="white--text">Mes documents</span></v-btn>
-              </v-list-item>
-              <v-list-item>
-                <v-btn color="#1e35b4" min-width="200"><span class="white--text">Mes statistiques</span></v-btn>
-              </v-list-item>
-              <v-list-item>
-                <v-btn color="#1e35b4" min-width="200" link :to="agendaPath"><span class="white--text">Mon agenda</span></v-btn>
-              </v-list-item>
-            </v-list>
+            <div class="my-2">membre depuis le 29/08/2018</div>
+           
+            <div class="my-2"><v-btn color="#1e35b4" min-width="200"><span class="white--text">Modifier la photo</span></v-btn></div>
+            <div class="my-2"><v-btn color="#1e35b4" min-width="200" link :to="docPath"><span class="white--text">Mes documents</span></v-btn></div>
+            <div class="my-2"><v-btn color="#1e35b4" min-width="200"><span class="white--text">Mes statistiques</span></v-btn></div>
+            <div class="my-2"><v-btn color="#1e35b4" min-width="200" link :to="agendaPath"><span class="white--text">Mon agenda</span></v-btn></div>
+            
           </v-col>
-          <v-col :cols="2"></v-col>
         </v-row>
       </v-container>
     </v-content>
