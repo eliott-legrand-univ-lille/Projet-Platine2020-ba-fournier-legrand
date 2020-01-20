ebe676bc8a1a360b100530e50b2a47d6ba1044ed
Eliott Legrand <e-legrand59@hotmail.fr>
2020-01-20 07:14:54 +0100

    update sportmenu css

--- open-asso-vuejs/src/components/sport/SportMenu.vue
+++ open-asso-vuejs/src/components/sport/SportMenu.vue
@@ -8,15 +8,15 @@
 
           <v-row>
             <v-col
-              class="d-flex flex-column"
+              class="pa3 d-flex flex-column"
               v-for="n in Menu.length"
               :key="n"
               cols="6"
               sm="4"
             >
-              <v-card class="d-flex justify-space-around align-center flex-column" 
+              <v-card class="flex d-flex justify-space-around align-center flex-column" 
               :color="Menu[n - 1].color" tile link :to="Menu[n-1].link">
-                <v-card-title primary-title style="word-break:normal" class="orange--text pb-1 pt-3 px-2">
+                <v-card-title primary-title style="text-align: center;word-break:keep-all" class=" align-center orange--text pb-1 pt-3 px-2 flex">
                   <h4 class="text-xs-center">{{Menu[n - 1].label}}</h4>
                 </v-card-title>
                 <v-icon color="white" x-large v-text="Menu[n - 1].icon"></v-icon>
