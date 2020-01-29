616a5b88923e909d7262da105b6543b716cb41b3
Ramata BA <ramata.ba.etu@univ-lille.fr>
2020-01-29 12:30:15 +0100

    Fin de la création d'évènement (quelques ajustement seront à faire mais pas urgents), mise en place du téléchargement de documents pour le profile (début)

--- open-asso-vuejs/src/components/profile/Documents.vue
+++ open-asso-vuejs/src/components/profile/Documents.vue
@@ -19,11 +19,23 @@
       </v-col>
     </v-row>
     <v-row justify="center">
-      <v-btn color="#1e35b4">
+      <v-btn color="#1e35b4" @click="newDoc = true">
         <span class="white--text">Ajouter un document</span>
       </v-btn>
     </v-row>
+      <v-dialog v-model="newDoc">
+        <v-card>
+        <v-card-title>Selectionnez le nouveau document à ajouter</v-card-title>
+        <v-card-text>
+        <v-file-input accept=".pdf,.jpeg,.png,.rtf,.jpg" label="File input"></v-file-input>
+        <v-btn @click="newDoc = false">Ajouter</v-btn>
+        <v-btn @click="newDoc=false">Annuler</v-btn>
+        </v-card-text>
+        </v-card>
+      </v-dialog>
   </v-container>
+
+
 </template>
 
 <script>
@@ -35,7 +47,13 @@ export default {
       "Carte d'identité",
       "Fiche d'inscription",
       "Certificat médical"
-    ]
-  })
+    ],
+    documentsFromBase: [],
+    newDoc : false,
+
+  }),
+  methods: {
+
+  }
 };
 </script>
