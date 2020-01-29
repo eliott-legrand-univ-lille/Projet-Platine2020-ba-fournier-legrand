1314d23a7b6d64deaedd8f1178db8cbee6e4fec7
Legrand Eliott <legrande@b11p13.fil.univ-lille1.fr>
2020-01-29 18:18:47 +0100

    finish Accounting Screen

--- open-asso-vuejs/src/components/admin/Accounting.vue
+++ open-asso-vuejs/src/components/admin/Accounting.vue
@@ -1,11 +1,12 @@
 <template>
     <v-container fluid>
+        <v-subheader>Résumé Comptabilité - De Septembre 2019 à Janvier 2020</v-subheader>
         <v-card
             class="mx-auto text-center"
             color="green"
             dark
             max-width="600"
-        >
+        >            
             <v-sheet color="rgba(0, 0, 0, .12)">
                 <v-sparkline
                     :value="value"
@@ -19,6 +20,20 @@
                 </v-sparkline>
             </v-sheet>
         </v-card>
+        <v-spacer></v-spacer>
+        <v-card class="mx-auto" max-width="600">
+            <v-subheader>Janvier 2020</v-subheader>
+            <v-list-item two-line>
+                <v-list-item-content>
+                    <v-list-item-title>Remboursement des sandwich des Déjeuners Sportifs</v-list-item-title>
+                    <v-list-item-subtitle>Le: 02/01/20</v-list-item-subtitle>
+                </v-list-item-content>
+                <v-list-item-action>
+                        <p>+5€</p>
+                    </v-list-item-action>
+            </v-list-item>
+        </v-card>
+        
     </v-container>
 </template>
 
@@ -26,18 +41,18 @@
 export default {
     data: () => ({
         labels: [
-        'Septembre',
-        'Octobre',
-        'Novembre',
-        'Décembre',
-        'Janvier'
+        '09/19: +223€',
+        '10/19: +242€',
+        '11/19: +275€',
+        '12/19: +210€',
+        '01/20: +290€'
         ],
         value: [
-            423,
-            442,
-            675,
-            510,
-            590
+            223,
+            242,
+            275,
+            210,
+            290
         ],      
     }),
 }
