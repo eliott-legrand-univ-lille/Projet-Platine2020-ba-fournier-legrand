a21a06ae546bcb4c27e705e2a4a432c54f08a2d6
Legrand Eliott <legrande@b11p13.fil.univ-lille1.fr>
2020-01-29 17:38:19 +0100

    add chart

--- open-asso-vuejs/src/components/admin/Accounting.vue
+++ open-asso-vuejs/src/components/admin/Accounting.vue
@@ -1,11 +1,44 @@
 <template>
-    <v-container>
-        
+    <v-container fluid>
+        <v-card
+            class="mx-auto text-center"
+            color="green"
+            dark
+            max-width="600"
+        >
+            <v-sheet color="rgba(0, 0, 0, .12)">
+                <v-sparkline
+                    :value="value"
+                    :labels="labels"
+                    color="rgba(255, 255, 255, .7)"
+                    height="100"
+                    padding="24"
+                    stroke-linecap="round"
+                    smooth
+                >
+                </v-sparkline>
+            </v-sheet>
+        </v-card>
     </v-container>
 </template>
 
 <script>
 export default {
-
+    data: () => ({
+        labels: [
+        'Septembre',
+        'Octobre',
+        'Novembre',
+        'Décembre',
+        'Janvier'
+        ],
+        value: [
+            423,
+            442,
+            675,
+            510,
+            590
+        ],      
+    }),
 }
-</script>
+</script>   
