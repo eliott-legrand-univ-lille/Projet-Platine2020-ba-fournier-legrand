44164ee114c6ce083e27e5023b9922fbe109f660
Eliott Legrand <e-legrand59@hotmail.fr>
2020-02-03 22:34:39 +0100

    update Events

--- open-asso-vuejs/src/components/events/Events.vue
+++ open-asso-vuejs/src/components/events/Events.vue
@@ -2,20 +2,18 @@
   <v-container>
     <v-row justify="center">
       <v-col>
-        <v-list subheader two-line v-for="(date, index) in allevents" :key="index">
-          <v-subheader v-text="date.date"></v-subheader>
-          <v-list-item v-for="(event, i) in date.events" :key="i" 
-            @click="setCurrentEventId(event.id)"
-          >
+        <v-list subheader two-line v-for="event in events" :key="event.id">
+          <!--<v-subheader v-text="date.date"></v-subheader>-->
+          <v-list-item @click="setCurrentEventId(event.id)">
             <v-list-item-avatar>
               <v-img style="background-color:orange;"></v-img>
             </v-list-item-avatar>
             <v-list-item-content>
-              <v-list-item-title v-text="event.title"></v-list-item-title>
-              <v-list-item-subtitle style="font-color:gray; font-size:12px" v-text="event.content"></v-list-item-subtitle>
+              <v-list-item-title v-text="event.name"></v-list-item-title>
+              <v-list-item-subtitle style="font-color:gray; font-size:12px" v-text="event.description"></v-list-item-subtitle>
             </v-list-item-content>
             <v-list-item-action>
-              <v-list-item-action-text v-text="event.hour"></v-list-item-action-text>
+              <v-list-item-action-text v-text="$moment(event.date).format('DD-MM-YY')"></v-list-item-action-text>
             </v-list-item-action>
 
 
@@ -34,8 +32,10 @@
     
 
     <ContentDialog 
-      :title=currentTitle 
-      :message=currentDescription
+      :name=currentTitle 
+      :description=currentDescription
+      :date=currentDate
+      :address=currentAddress
       :dialog.sync="dialog"
       :color="$route.meta.color"
     ></ContentDialog>
@@ -45,11 +45,11 @@
 <script>
 import paths from "@/routes/paths.js";
 import { db } from "@/db.js";
-import ContentDialog from "@/components/dialogue/ContentDialog.vue";
+import ContentDialog from "@/components/events/EventsDialog.vue";
 
 export default {
   firestore: {
-    events: db.collection("events")
+    events: db.collection("events").orderBy("date", "desc")
   },
   components: {
     ContentDialog
@@ -59,27 +59,6 @@ export default {
     events: [],
     createEvPath: paths.createevent.path,
     currentEventId: null,
-    allevents: [
-      {
-        date: "Aujourd'hui",
-        events: [
-          {
-            id: 1, title: "Projection match", hour: "19h-20", content: "Event 1", img: "img_link"  
-          },
-          {
-            id: 2, title: "Projection match", hour: "19h-20", content: "Event 2", img: "img_link"          
-          }
-        ]
-      },
-      {
-        date: "Demain",
-        events: [
-            { 
-              id: 3, title: "Projection match", hour: "19h-20", content: "Event 3", img: "img_link"
-            }
-          ]
-      }
-    ]
   }),
   methods: {
       setCurrentEventId(id) {
@@ -91,29 +70,41 @@ export default {
     // trigger when an event is clicked and return the content of the event clicked 
     currentEvent() {
       if (this.currentEventId !== null) {
-        for(var dateIt=0;dateIt<this.allevents.length;dateIt++){
-          for(var eventIt=0; eventIt<this.allevents[dateIt].events.length; eventIt++){
-            if(this.allevents[dateIt].events[eventIt].id===this.currentEventId){
-              return this.allevents[dateIt].events[eventIt];
+
+          for(var eventIt=0; eventIt<this.events.length; eventIt++){
+            if(this.events[eventIt].id===this.currentEventId){
+              return this.events[eventIt];
             }
           }
-        }
       }
       return null;
     },
     currentDescription() {
       if (this.currentEvent !== null) {
-          return this.currentEvent.content;
+          return this.currentEvent.description;
       }
       return null;
     },
     currentTitle() {
         if (this.currentEvent !== null) {
-            return this.currentEvent.title;
+            return this.currentEvent.name;
         }
         return null;
-      }
+    },
+    currentAddress() {
+        if (this.currentEvent !== null) {
+            return this.currentEvent.address + ", " + 
+            this.currentEvent.postal + " " + this.currentEvent.city;
+        }
+        return null;
+    },
+    currentDate() {
+        if (this.currentEvent !== null) {
+            return this.$moment(this.currentEvent.date).format('DD/MM/YYYY');
+        }
+        return null;
     }
+  },
 
 };
 </script>
---
+++ open-asso-vuejs/src/components/events/EventsDialog.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-row justify="center">
+        <v-dialog @click:outside="closeDialog" v-model="dialog">
+            <v-card>
+                <v-toolbar dark :color="color">
+                    <v-spacer></v-spacer>
+                    <v-btn icon dark @click="closeDialog()">
+                        <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                </v-toolbar>
+                <v-list-item two-line>
+                    <v-list-item-content>
+                        <h4 style="word-break:normal" class="text-center headline mb-1">{{ name }}</h4>
+                        <v-divider></v-divider>
+                        <p>Description: {{ description }}</p>
+                        <p></p>
+                        <p>Le: {{ date }}</p>
+                        <p></p>
+                        <p>À: {{address}}</p>
+
+                    </v-list-item-content>
+                </v-list-item>
+            </v-card>
+        </v-dialog>
+    </v-row>  
+</template>
+<script>
+export default {
+    props: {
+        name: String,
+        description: String,
+        color: String,
+        date: String,
+        address: String,
+        dialog: Boolean
+    },
+    methods: {
+        closeDialog () {
+            this.$emit('update:dialog', false)
+        }
+    },
+}
+</script>
